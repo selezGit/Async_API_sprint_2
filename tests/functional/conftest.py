3c8a6dc5c3b0f1b86f4d223e53f31ede0e24ecaa
selezGit <pwselezard@gmail.com>
2021-05-06 02:14:10 +0300

    Переделал структуру тестов
    1. Поставил 0.12 версию pytest_asyncio

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -21,8 +21,8 @@ async def es_client():
 
 @pytest.fixture(scope='session')
 async def session():
-    session = await aiohttp.ClientSession()
-    yield session
+    async with aiohttp.ClientSession() as session:
+        yield session
     await session.close()
 
 
@@ -32,9 +32,8 @@ async def redis_client():
     yield client
     await client.close()
 
-
 @pytest.fixture
-def make_get_request(session):
+async def make_get_request(session):
     async def inner(method: str, params: dict = None) -> HTTPResponse:
         params = params or {}
         # в боевых системах старайтесь так не делать!
--- tests/functional/src/test_s.py
+++ tests/functional/src/test_s.py
@@ -1,17 +1,13 @@
 import pytest
-import repackage
-
-repackage.up()
-
-from conftest import make_get_request
 
 @pytest.mark.asyncio
-async def test_search_detailed(es_client):
+async def test_search_detailed(es_client, make_get_request):
     # Заполнение данных для теста
-    await es_client.bulk(...)
-
+    # await es_client.bulk(...)
+    print('here')
     # Выполнение запроса
     response = await make_get_request('/search', {'search': 'Star Wars'})
+    print(response)
     # Проверка результата
     assert response.status == 200
     assert len(response.body) == 1
