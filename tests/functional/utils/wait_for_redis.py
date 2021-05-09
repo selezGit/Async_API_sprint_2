cf142d44e5da9d724211e01a8493a4b9cc74d0a6
Asman Malikov <asmanmalikov@gmail.com>
2021-05-09 17:47:07 +0300

    добавил ожидание еластика и редиса перед запуском тестов

--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,1 +1,24 @@
-# функция ожидания запуска redis например она будет работать в цикле пока не получит ответ на пинг
+import aioredis_cluster
+import asyncio
+
+
+async def wait_redis():
+    client = await aioredis_cluster.create_redis_cluster(["redis://redis-node-0",
+                              "redis://redis-node-1",
+                              "redis://redis-node-2",
+                              "redis://redis-node-3",
+                              "redis://redis-node-4",
+                              "redis://redis-node-5",
+                              ])
+    response = await client.ping()
+    while not response:
+        await asyncio.sleep(2)
+        print("Redis-Cluster is unavailable - sleeping")
+        response = await client.ping()
+    #await client.close()
+    print("Redis-Cluster is run")
+
+
+if __name__ == '__main__':
+    asyncio.run(wait_redis())
+
