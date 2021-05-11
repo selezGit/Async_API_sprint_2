94029582b96fc374804dab6394d39df92a06acb2
selezGit <pwselezard@gmail.com>
2021-05-11 15:40:36 +0300

    Написал ещё пару тестов

--- tests/functional/src/test_s.py
+++ tests/functional/src/test_s.py
@@ -2,6 +2,8 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_search_detailed(prepare_es_film, make_get_request):
+    """Поиск конкретного фильма"""
+
     # Выполнение запроса
     response = await make_get_request('/film', {'query': 'abracadabra'})
     # Проверка результата
@@ -10,3 +12,27 @@ async def test_search_detailed(prepare_es_film, make_get_request):
     assert len(response.body) == 1
 
     assert response.body == prepare_es_film
+
+@pytest.mark.asyncio
+async def test_get_by_id(prepare_es_film, make_get_request):
+    """Тест проверяет работу получения по id в эндпоинте film"""
+
+    response = await make_get_request('/film/3a5f9a83-4b74-48be-a44e-a6c8beee9460')
+
+    # Проверка результата
+    assert response.status == 200
+
+    assert response.body == prepare_es_film[0]
+
+@pytest.mark.asyncio
+async def test_get_all_films(make_get_request, get_all_data_elastic):
+    """Вывести все фильмы"""
+
+    #получаем все фильмы из elasticsearch    
+    all_films= await get_all_data_elastic('movies')
+
+    response = await make_get_request('/film', {'size': 1000, 'page': 1})
+
+    assert response.status == 200
+
+    assert len(response.body) == len(all_films)
